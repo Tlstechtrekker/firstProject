1b04a85f04c4e7c95dbeb4b46e9d6fdcf88f5e8b
Tlstechtrekker <tschultzphd@gmail.com>
2021-12-21 00:40:41 -0700

    set top navbar bkgr to white-added color coding of borders for dev support

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -4,18 +4,27 @@ import styled from 'styled-components';
 //example of how granular you can get with styled components; don't need to get this detailed
 
 const NavbarStyled = styled.nav`
-    background: yellow;
-    color: pink;
+    background: #fff;
+    border: 1px solid #000;
+    padding: 15px 50px;
+    font-size: .75rem;
+    font-weight: 600;
+    text-transform: uppercase;
     /* width: 60%; */
 `
 
 const MenuItems = styled.ul`
+    background: #fff;
     display: flex;
     flex-direction: row;
     list-style-type: none;
+    border: 1px solid red;
+    justify-content: center;
 `
 
 const MenuItem = styled.li`
+    color: #060;
+    background: yellow;
     margin-right: 25px;
 `
 
